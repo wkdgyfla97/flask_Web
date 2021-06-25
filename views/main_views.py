60fa74479a8813dea6a3ad80d87820dbbcb0c043
wkdgyfla97 <wkdgyfla97@naver.com>
2021-06-25 22:03:24 +0900

    flask test in 25 06 2021

---
+++ views/main_views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, url_for
+from werkzeug.utils import redirect
+
+from pybo.models import Question
+
+bp = Blueprint('main', __name__, url_prefix='/')
+@bp.route('/hello')
+def hello_pybo():
+    return "Hello JoRim!"
+
+@bp.route('/')
+def index():
+    return redirect(url_for('question._list'))
+
+# @bp.route("/")
+# def index():
+#     question_list = Question.query.order_by(Question.create_date.desc())
+#     return render_template('question/question_list.html', question_list=question_list)
+#
+# @bp.route('/detail/<int:question_id>/')
+# def detail(question_id):
+#     question = Question.query.get_or_404(question_id)
+#     return render_template('question/question_detail.html', question=question)
