c752f3783a9fefba10de3e5542e1e5439a711e7d
wkdgyfla97 <wkdgyfla97@naver.com>
2021-06-28 17:02:17 +0900

    회원가입 성공

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+
 
 class QuestionForm(FlaskForm):
     subject = StringField('제목', validators=[DataRequired("제목 입력해주세요")])
@@ -8,3 +10,9 @@ class QuestionForm(FlaskForm):
 
 class AnswerForm(FlaskForm):
     content = TextAreaField('내용', validators=[DataRequired('답글도 내용 좀 적어주셈')])
+
+class UserCreateForm(FlaskForm):
+    username = StringField('사용자 이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password1 = PasswordField('비밀번호', validators=[DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다.')])
+    password2 = PasswordField('비밀번호 확인', validators=[DataRequired()])
+    email = EmailField('이메일', [DataRequired(), Email()])
